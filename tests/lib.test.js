23399ad10231df6d972cfc823402d75a85951544
Bela Jash <abelxotessema@gmail.com>
2024-08-25 11:22:46 +0300

    Testing Numbers.

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,3 +1,16 @@
-test('Our first test', () => {
-  throw new Error('Something went wrong.');
+const lib = require('../lib');
+
+test('absolute - should return a positive number if input is positive', () => {
+  const result = lib.absolute(1);
+  expect(result).toEqual(1);
+});
+
+test('absolute - should return a positive number if input is negative', () => {
+  const result = lib.absolute(-1);
+  expect(result).toEqual(1);
+});
+
+test('absolute - should return 0 if input is 0', () => {
+  const result = lib.absolute(0);
+  expect(result).toEqual(0);
 });
