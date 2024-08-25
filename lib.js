b9552111357b88e1f16ae57dde55d2f422c9bcff
Bela Jash <abelxotessema@gmail.com>
2024-08-25 16:17:15 +0300

    Interaction Testing.

--- lib.js
+++ lib.js
@@ -1,5 +1,5 @@
 const db = require('./db');
-const sendMail = require('./mail');
+const mail = require('./mail');
 
 // Testing numbers
 module.exports.absolute = function(number) {
@@ -42,4 +42,10 @@ module.exports.applyDiscount = function(order) {
   
   if (customer.points > 10)
     order.totalPrice *= 0.9;
+}
+
+module.exports.notifyCustomer = function(order) {
+  const customer = db.getCustomerSync(order.customerId);
+  
+  mail.send(customer.email, 'Your order was placed successfully.');
 }
