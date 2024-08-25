d4d3cfe7209a3224f5f8ae30374b39207fb0821d
Bela Jash <abelxotessema@gmail.com>
2024-08-25 12:02:37 +0300

    Testing Objects.

--- lib.js
+++ lib.js
@@ -23,3 +23,8 @@ module.exports.greet = function(name) {
 module.exports.getCurrencies = function() {
   return ['USD', 'AUD', 'EUR'];
 }
+
+// Testing objects
+module.exports.getProduct = function(productId) {
+  return {id: productId, price: 10};
+}
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -48,3 +48,13 @@ describe('getCurrencies', () => {
     expect(result).toEqual(expect.arrayContaining(['EUR', 'USD', 'AUD']));
   });
 });
+
+describe('getProduct', () => {
+  it('should return a product with the given id', () => {
+    const result = lib.getProduct(1);
+    expect(result).toEqual({id: 1, price: 10}); // Too specific
+    expect(result).toMatchObject({id: 1});
+    expect(result).toHaveProperty('id');
+    expect(result).toHaveProperty('id', 1);
+  });
+});
